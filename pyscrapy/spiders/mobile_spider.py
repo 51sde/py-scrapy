271ab344f7db238a0879d0f415bdfd078893219b
Chory <chorys@qq.com>
2017-06-07 23:45:04 +0800

    mobile spider

--- pyscrapy/spiders/mobile_spider.py
+++ pyscrapy/spiders/mobile_spider.py
@@ -1,18 +1,17 @@
 #-*-coding:utf-8-*-
 
 import scrapy
+import gzip
 
 
 class MobileSpider(scrapy.Spider):
     name = "mobile"
     allowed_domains = ["ip138.com"]
     start_urls = [
-        "http://www.ip138.com:8080/search.asp?action=mobile&mobile=18701766310"
+        "http://www.ip138.com:8080/search.asp?action=mobile&mobile=18701760000"
     ]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2]
-        print '你好，世界'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-
+        for c in response.xpath('//td[@class="tdc2"]'):
+            data = c.xpath("text()").extract()
+            print data[0]
