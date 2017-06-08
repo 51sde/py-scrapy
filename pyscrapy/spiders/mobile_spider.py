02628676b49bd07054d03c59c36c1cd55b3548aa
Chory <chorys@qq.com>
2017-06-08 22:54:54 +0800

    批量获取手机号码

--- pyscrapy/spiders/mobile_spider.py
+++ pyscrapy/spiders/mobile_spider.py
@@ -1,17 +1,28 @@
 #-*-coding:utf-8-*-
 
 import scrapy
-import gzip
-
+from scrapy.http import Request
 
 class MobileSpider(scrapy.Spider):
     name = "mobile"
     allowed_domains = ["ip138.com"]
     start_urls = [
-        "http://www.ip138.com:8080/search.asp?action=mobile&mobile=18701760000"
+        "http://localhost"
     ]
 
     def parse(self, response):
+        urls = [
+            "http://www.ip138.com:8080/search.asp?action=mobile&mobile=1870176",
+            "http://www.ip138.com:8080/search.asp?action=mobile&mobile=1380539",
+            "http://www.ip138.com:8080/search.asp?action=mobile&mobile=1718538",
+        ]
+        for url in urls:
+            yield Request(url, callback=self.parse_item)
+
+    def parse_item(self, response):
         for c in response.xpath('//td[@class="tdc2"]'):
             data = c.xpath("text()").extract()
             print data[0]
+
+    def a(self):
+        pass
